fc83ba1ceced4a4752a6ebe538e0a6105145e53d
DESKTOP-Q7R713D\Vadym <vadim.nakitnyak@gmail.com>
2020-12-04 20:48:10 +0200

    Set displayName field to unique.

--- src/repository/Users.kt
+++ src/repository/Users.kt
@@ -6,6 +6,6 @@ import org.jetbrains.exposed.sql.Table
 object Users : Table() {
     val userId : Column<Int> = integer("id").autoIncrement().primaryKey()
     val email = varchar("email", 128).uniqueIndex()
-    val displayName = varchar("display_name", 256)
+    val displayName = varchar("display_name", 256).uniqueIndex()
     val passwordHash = varchar("password_hash", 64)
 }
